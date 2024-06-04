bb7480ab17ba2df51a0e1453a0fafa209a0d40cc
BhuvanO7 <93661418+BhuvanO7@users.noreply.github.com>
2024-06-05 00:09:13 +0530

    first commit

---
+++ home/footer.php
@@ -0,0 +1,27 @@
+<!-- copyright and contactUs fixed footer -->
+
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+.footer {
+    position: fixed;
+    width: 100%;
+    text-align: center;
+    
+    bottom: 0;
+    background-color: black;
+    color: white;
+}
+</style>
+</head>
+<body>
+<footer class="footer">
+
+  <p style="margin-top: 4px;margin-bottom:-10px">Copyright © 2024 Designed By Team TITAN'S</p><br>
+  <a href="mailto:titans@gmail.com" style="color:white">ContactUs</a>
+
+</footer>
+
+</body>
+</html>
